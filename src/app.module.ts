df1595a87c8e03d1c2f5ba14f74d23a79483bf6e
Gabriel Fernandes <contato.gfernandesribeiro@gmail.com>
2022-10-30 15:26:12 -0300

    Remove unused default Nest code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { SongModule } from './song/song.module';
 import { ArtistModule } from './artist/artist.module';
 
 @Module({
   imports: [SongModule, ArtistModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
