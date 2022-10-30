d0244d790ffbac4d3c2eb0594b35844e1a13e84f
Gabriel Fernandes <contato.gfernandesribeiro@gmail.com>
2022-10-30 16:29:42 -0300

    Refactor artist service to extract private function to get artist songs

--- src/artist/artist.service.ts
+++ src/artist/artist.service.ts
@@ -1,4 +1,5 @@
 import { HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
+import { Artist, Song } from '@prisma/client';
 import { PrismaService } from '../prisma.service';
 import { ArtistResponseDto } from './dtos/ArtistResponse.dto';
 import { CreateArtistDto } from './dtos/CreateArtist.dto';
@@ -7,14 +8,18 @@ import { CreateArtistDto } from './dtos/CreateArtist.dto';
 export class ArtistService {
   constructor(private readonly prisma: PrismaService) {}
 
+  private async getArtistSongs(artist: Artist): Promise<Song[]> {
+    return this.prisma.song.findMany({
+      where: { artists: { some: { artist_id: artist.id } } },
+    });
+  }
+
   async getAllArtists(): Promise<ArtistResponseDto[]> {
     const artists: ArtistResponseDto[] = [];
     const artistEntities = await this.prisma.artist.findMany();
 
     for (const artistEntity of artistEntities) {
-      const artistSongs = await this.prisma.song.findMany({
-        where: { artists: { some: { artist_id: artistEntity.id } } },
-      });
+      const artistSongs = await this.getArtistSongs(artistEntity);
 
       artists.push(ArtistResponseDto.fromEntites(artistEntity, artistSongs));
     }
@@ -32,10 +37,7 @@ export class ArtistService {
       });
     }
 
-    const songEntities = await this.prisma.song.findMany({
-      where: { artists: { some: { artist_id: artistEntity.id } } },
-    });
-
+    const songEntities = await this.getArtistSongs(artistEntity);
     return ArtistResponseDto.fromEntites(artistEntity, songEntities);
   }
 
