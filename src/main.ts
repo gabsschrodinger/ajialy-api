2a0acd27cbb4024e6fc27c2702436626ce6be7f5
Gabriel Fernandes <contato.gfernandesribeiro@gmail.com>
2022-10-31 00:29:50 -0300

    Adds swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
+
+  const config = new DocumentBuilder()
+    .setTitle('Ajialy API')
+    .setDescription('Docs for the Ajialy REST API')
+    .setVersion(process.env.npm_package_version)
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
